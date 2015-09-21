3c27247a2234985356b40aae96d822c539f0547a
cigreja <cigreja@live.com>
2015-09-21 12:09:35 -0400

    - change combobox options for batch editor **DONE**  ** tasks **  - programmer  - rk  - dateAssigned  - notes  - done  - dateDone  ** task files **  - submitter  - date  - path  - notes  ** task notes **  - submitter  - status_notes  - date

--- src/com/elle/ProjectManager/logic/ITableConstants.java
+++ src/com/elle/ProjectManager/logic/ITableConstants.java
@@ -32,7 +32,7 @@ public interface ITableConstants {
     public static final String[] TASKNOTES_SEARCH_FIELDS = {"submitter"};
     
     // batch edit combobox selections for each table
-    public static final String[] TASKS_BATCHEDIT_CB_FIELDS = {"programmer", "rank", "dateAssigned", "notes", "done", "dateDone"};
+    public static final String[] TASKS_BATCHEDIT_CB_FIELDS = {"programmer", "rk", "dateAssigned", "notes", "done", "dateDone"};
     public static final String[] TASKFILES_BATCHEDIT_CB_FIELDS = {"submitter", "date", "path", "notes"};
     public static final String[] TASKNOTES_BATCHEDIT_CB_FIELDS = {"submitter", "status_notes", "date"};
 }
