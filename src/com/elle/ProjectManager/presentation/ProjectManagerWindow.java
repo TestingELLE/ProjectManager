84d07fc56970007de19dcd327049b5a5a279ffb7
cigreja <cigreja@live.com>
2015-10-01 13:29:36 -0400

    ProjectManager-0.9.0 *** NEW JAR ***   - button enabled behavior determined by editing states   - edit mode text is now green when editing mode is on
    - update all features from Analyster to ProjectManager
      - see notes from Wednesday 23 Sept. -> Thursday 1 Oct. for all the detailed notes on the features implemented
    
    check list 
     - removed the panel for batch edit buttons
     - added color variables for the edit mode text
     - initialize colors and change naming conventions
     - hide button revert changes and refactor the name
     - implemented switch button code and required methods that were added, as well as update the tab class
     - double right mouse click enter edit mode code implemented
       - notes- I noticed the code for the left mouse double click and single click which I hope does not break functionality (I have not tested)
       - update: the code appears to work fine when I tested. The double click filter feature works as long as no text pop up is associated with that cell.
     - changeTabbedPaneState method code implemented 
     - upload changes code implemented
     - revert changes code implemented
     - batch edit button code implemented
     - initialize all tabs as not in an editing mode active state
     - makeTableEditable code updated
     - setKeyboardFocusManager code updated
     - batch edit quit button code implemented
     - tableModelListener code updated
     - single left mouse click feature code updated
       - this just selects the cell ( you can start typing or use double right click to enter cell and select all text)
     - removed line of code from LoginWindow - set window not visible when object is created
      - I usually set visible when I create an object. That is the way that the java api seems to do it.
        - example create a new JFrame and you won't see it until you put setVisible to true because it should be false by default.

--- src/com/elle/ProjectManager/presentation/ProjectManagerWindow.java
+++ src/com/elle/ProjectManager/presentation/ProjectManagerWindow.java
@@ -51,8 +51,8 @@ import java.util.Vector;
 public class ProjectManagerWindow extends JFrame implements ITableConstants {
 
     // Edit the version and date it was created for new archives and jars
-    private final String CREATION_DATE = "2015-09-23";
-    private final String VERSION = "0.8.8a";
+    private final String CREATION_DATE = "2015-10-01";
+    private final String VERSION = "0.9.0";
 
     // attributes
     private Map<String, Tab> tabs; // stores individual tabName information
