27e0ca57a5bd0281b187d226e7abc92e15c0a8ec
cigreja <cigreja@live.com>
2015-10-02 14:14:16 -0400

    ProjectManager-0.9.1 *** NEW JAR ***  - Batch edit button does not change the upload/revert options  - batch edit window now shows always on top of the main window  - edit Mode text for other tabs that are not editing ( editing tabNameHere ... )    - this also displays in green when editing is active  - addrecords button creates a new window if not opened and sets focus to top is a window is already open

--- src/com/elle/ProjectManager/presentation/ProjectManagerWindow.java
+++ src/com/elle/ProjectManager/presentation/ProjectManagerWindow.java
@@ -51,8 +51,8 @@ import java.util.Vector;
 public class ProjectManagerWindow extends JFrame implements ITableConstants {
 
     // Edit the version and date it was created for new archives and jars
-    private final String CREATION_DATE = "2015-10-01";
-    private final String VERSION = "0.9.0";
+    private final String CREATION_DATE = "2015-10-02";
+    private final String VERSION = "0.9.1";
 
     // attributes
     private Map<String, Tab> tabs; // stores individual tabName information
