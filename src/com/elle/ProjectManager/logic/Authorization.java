fa9f8ddf18b09fad1dd5e385f651195eae5003a9
cigreja <cigreja@live.com>
2016-01-22 06:41:44 -0500

    abstract Authorization class - added comments and param to authorize

--- src/com/elle/ProjectManager/logic/Authorization.java
+++ src/com/elle/ProjectManager/logic/Authorization.java
@@ -1,7 +1,11 @@
 package com.elle.ProjectManager.logic;
 
+import java.awt.Component;
+
 /**
- *
+ * This class will simply override any original behavior depending on the 
+ * user authorization level. Obviously the highest level will have full
+ * access and no behavior would be 
  * @author Carlos Igreja
  */
 public abstract class Authorization {
@@ -16,6 +20,19 @@ public abstract class Authorization {
     // ADMINISTRATOR = ?
     // DEVELOPER = ?
     
+    /**
+     * When the user logs in, we will need to know the access level and 
+     * restrict the application accordingly per user type or access level.
+     * This information will be stored and retrieved from the database.
+     * This method will get the required information from the database.
+     */
     public abstract void getInfoFromDB();
-    public abstract void authorize();
+    /**
+     * This takes any component and overrides any behavior for that component.
+     * Example c instanceOf JFrame then is PM, issue window etc. and regulate
+     * the behavior as needed. If full access no checks required, exit promptly.
+     * Default is full access and restrictions may apply per user type.
+     * @param c 
+     */
+    public abstract void authorize( Component c);
 }
