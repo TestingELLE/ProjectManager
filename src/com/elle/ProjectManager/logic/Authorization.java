604ac4cde94258d16ef2b94bb887bdd5cd881ffc
cigreja <cigreja@live.com>
2016-01-26 06:43:44 -0500

    add implementation for developer restrictions / permissions

--- src/com/elle/ProjectManager/logic/Authorization.java
+++ src/com/elle/ProjectManager/logic/Authorization.java
@@ -1,24 +1,31 @@
 package com.elle.ProjectManager.logic;
 
+import com.elle.ProjectManager.database.DBConnection;
+import com.elle.ProjectManager.presentation.ProjectManagerWindow;
 import java.awt.Component;
 
 /**
  * This class will simply override any original behavior depending on the 
- * user authorization level. Obviously the highest level will have full
- * access and no behavior would be 
+ * user authorization level. The highest level will have full access and no 
+ * behavior would be altered. Restrictions can apply accordingly for each
+ * access level type. This class will be static because it will be used 
+ * throughout the application.
  * @author Carlos Igreja
  */
-public abstract class Authorization {
+public class Authorization {
     
     // Information will probably come from the database at start up
     
     // user information
-    // userid
-    // userLevel
+    // userid - LOGIN (pupone_Carlos) - issue can't access users table if exists?
+    // userLevel - Admin or Developer
+    private static String userLogin;
+    private static int userLevel;
     
     // constants
-    // ADMINISTRATOR = ?
-    // DEVELOPER = ?
+    private static final int ADMINISTRATOR = 1;
+    private static final int DEVELOPER = 2;
+
     
     /**
      * When the user logs in, we will need to know the access level and 
@@ -26,7 +33,12 @@ public abstract class Authorization {
      * This information will be stored and retrieved from the database.
      * This method will get the required information from the database.
      */
-    public abstract void getInfoFromDB();
+    public static void getInfoFromDB(){
+        userLogin = DBConnection.getUserName();
+        // use sql query to get the userLevel from DB
+        userLevel = 2; // DB needs to be implemented
+    }
+    
     /**
      * This takes any component and overrides any behavior for that component.
      * Example c instanceOf JFrame then is PM, issue window etc. and regulate
@@ -34,5 +46,30 @@ public abstract class Authorization {
      * Default is full access and restrictions may apply per user type.
      * @param c 
      */
-    public abstract void authorize( Component c);
+    public static void authorize( Component c){
+        
+        switch(userLevel){
+            case ADMINISTRATOR:
+                break;
+            case DEVELOPER:
+                developerPermissions(c);
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * Developer Restrictions
+     * @param c The component to be authorized (restrict features or behavior)
+     */
+    private static void developerPermissions(Component c) {
+        
+        // ProjectManagerWindow
+        if(c instanceof ProjectManagerWindow){
+            ProjectManagerWindow pm = (ProjectManagerWindow)c;
+            // menu item components
+            pm.getMenuItemDummy().setEnabled(false);
+        }
+    }
 }
