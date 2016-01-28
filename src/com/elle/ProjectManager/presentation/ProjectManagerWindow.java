d8d378ef9ee0eeedbd698699ee2507abd98c46ee
cigreja <cigreja@live.com>
2016-01-28 12:33:55 -0500

    new jar 1.0.9

--- src/com/elle/ProjectManager/presentation/ProjectManagerWindow.java
+++ src/com/elle/ProjectManager/presentation/ProjectManagerWindow.java
@@ -52,8 +52,8 @@ import javax.imageio.ImageIO;
 public class ProjectManagerWindow extends JFrame implements ITableConstants {
 
     // Edit the version and date it was created for new archives and jars
-    private final String CREATION_DATE = "2016-01-26";
-    private final String VERSION = "1.0.8";
+    private final String CREATION_DATE = "2016-01-28";
+    private final String VERSION = "1.0.9";
 
     // attributes
     private Map<String, Tab> tabs; // stores individual tabName information
