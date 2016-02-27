ead61ddcaf20f861ce9854e5a4e5baff5530e63f
silencelot <silencelot@youzhis-mbp.home>
2016-02-27 10:39:57 -0500

    update Addissuewindow design; update reload tab data and all data;

--- src/com/elle/ProjectManager/logic/ITableConstants.java
+++ src/com/elle/ProjectManager/logic/ITableConstants.java
@@ -15,7 +15,7 @@ package com.elle.ProjectManager.logic;
 public interface ITableConstants {
     
     // Edit the version and date it was created for new archives and jars
-    public final String CREATION_DATE = "2016-02-25";
+    public final String CREATION_DATE = "2016-02-27";
     public final String VERSION = "1.2.0";
     
     public static final String TASKS_TABLE_NAME = "issues";
