f347cfffa0b7061346f9c4630bc5f2debca6469b
cigreja <cigreja@live.com>
2016-04-09 14:31:49 -0400

    New Version - 1.7 : IssueWindow Refactoring

--- src/com/elle/ProjectManager/logic/ITableConstants.java
+++ src/com/elle/ProjectManager/logic/ITableConstants.java
@@ -15,8 +15,8 @@ package com.elle.ProjectManager.logic;
 public interface ITableConstants {
     
     // Edit the version and date it was created for new archives and jars
-    public final String CREATION_DATE = "2016-03-29";
-    public final String VERSION = "1.5.0";
+    public final String CREATION_DATE = "2016-4-9";
+    public final String VERSION = "1.7.0";
     
     public static final String TASKS_TABLE_NAME = "issues";
     public static final String TASKFILES_TABLE_NAME = "issue_files";
