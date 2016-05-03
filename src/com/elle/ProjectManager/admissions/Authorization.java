132bd8931a17c301e44761188af3e1e4a5ab2014
cigreja <cigreja@live.com>
2016-05-03 15:23:48 -0400

    added needed operation that I missed

--- src/com/elle/ProjectManager/admissions/Authorization.java
+++ src/com/elle/ProjectManager/admissions/Authorization.java
@@ -55,6 +55,7 @@ public class Authorization {
         }
         else{
             accessLevel = USER; // defaults to user
+            setAdminComponentType(accessLevel);
             return false;
         }
     }
